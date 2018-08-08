09acfa9eab4b4b78dcc1592504b0e57a0f166414
mateuszr87 <mateuszr87@gmail.com>
2018-08-08 22:14:03 +0200

    add ssl ceriticate and enable https

--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,17 @@
 import app from "./app";
+import * as https from'https';
+import * as fs from 'fs';
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log('server running on port ' + PORT);
-});
+const httpsOptions = {
+    key: fs.readFileSync('./config/key.pem'),
+    cert: fs.readFileSync('./config/cert.pem')
+}
+
+https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log('Secure server runing');
+})
+
+// app.listen(PORT, () => {
+//     console.log('server running on port ' + PORT);
+// });
